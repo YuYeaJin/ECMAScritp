70d843a859e69f14988f6e7886a3d1f425c81d4a
YuYeaJin <yyj9290@naver.com>
2024-12-09 11:58:03 +0900

    오전

---
+++ 01-basic.js
@@ -0,0 +1,24 @@
+// 한 줄 주석
+/*
+    여러 줄 주석
+*/
+
+// 콘솔 출력은 전역 객체 console의 출력 메서드 사용
+console.error("에러 메시지");
+console.warn("경고 메시지");
+console.debug("디버그 메시지");
+console.log("일반 메시지");
+console.info("정보 메시지");
+
+// 내용을 연속적으로 출력할 때는 ,로 구문
+console.log("ECMAScript", 2024, true);
+
+console.log(process);
+
+// 객체의 속성에 접근할 때는 . dmfh wjqrms
+
+console.log("Process Version:", process.Version);
+console.log("Process Platform:", process.platform);
+console.log("Process Architecture:", process.arch);
+
+console.log("==================");

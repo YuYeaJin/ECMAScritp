56ce6f88ebc3ca69e2af39ae97b664b5b9df1486
YuYeaJin <yyj9290@naver.com>
2024-12-16 14:39:53 +0900

    잘해보자..

---
+++ ex03-rest-api/js/app.js
@@ -0,0 +1,83 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const apiUrl = "http://localhost:3000/todos"; // API Endpoint URL
+    const todoList = document.getElementById("todo-list"); // TODO list element: ul#todo-list
+    const newTodoInput = document.getElementById("new-todo"); // New TODO input element: input#new-todo
+    const addTodoButton = document.getElementById("add-todo"); // Add TODO button element: button#add-todo
+
+    // Add TODO to DOM
+    //  새로운 todo item을 ul#todo-list에 추가하는 함수
+    function addTodoToDOM(todo) {
+        //  ul#todo-list에 자식 li 노드 추가
+        const li = document.createElement("li");
+        // 콘텐츠 설정
+        li.innerHTML = `
+            <span>${todo.title}</span>
+            <button onclick="deleteTodo(${todo.id})">Delete</button>
+        `;
+        li.className = "todo-ltem";
+        todoList.appendChild(li);
+    }
+
+    // Fetch and display existing TODOs
+    //  API로부터 todo list를 가져와서 화면에 표시하는 함수
+    function fetchTodos() {
+        fetch(apiUrl)   // get 방식
+            .then(response => response.json())
+            .then(data => {
+                //  기존의 todoList 비우기
+                todoList.innerHTML = "";
+
+                console.log("data:", data);
+                data.forEach(todo => {
+
+                    //   아이템을 DOM에 추가
+                    addTodoToDOM(todo);
+                });
+            })
+    }
+
+    // Add new TODO
+    //  새로운 todo를 추가하는 이벤트 핸들러 연결
+    addTodoButton.addEventListener("click", (event) => {
+        //  #new-todo 입력 상자의 값을 가지고 와서
+        //  새 todo item 항목을 만들어 서버로 전송(post 방식)
+        const title = newTodoInput.ariaValueMax.trim();
+        if (title) {
+            //  타이틀이 비어있지 않으면 서버로 전송
+            fetch(apiUrl, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    tltle: title,
+                    completed: false
+                })
+            })
+            .then((response) => response.json())
+            .thes(todo => {
+                addTodoToDOM(todo);
+                newTodoInput.value = "";
+            })
+            .catch(error => console.error("Error adding TODO:", error))
+        }
+        newTodoInput.fucos();
+    });
+
+    // Delete TODO
+    //  특정 todo를 삭제하는 함수 (삭제 버튼의 클릭 이벤트 핸들러로 사용됨)
+    window.deleteTodo = function (id) {
+        // Endpoint : http://localhost:3000/todos
+        fetch(`${apiUrl}/${id}`, {
+            method: "DELETE"
+        })
+        .then(() => {
+            // 리스트 다시 생성
+            fetchTodos();
+        })
+        .catch(error => console.error("Error delete TODO", error));
+    };
+
+    // Initial fetch
+    fetchTodos();
+});
